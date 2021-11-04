74cf8c1dde5f3f621a670cba115d52b5000e1480
Linh Hoang <ellehoang@hotmail.com>
2021-11-04 09:24:10 -0400

    add setupInput functions from play.js and implement setupInput module for export

---
+++ input.js
@@ -0,0 +1,21 @@
+const setupInput = function () {
+  
+  const stdin = process.stdin; // allows us to listen to keyboard input and react to it
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  
+  const handleUserInput = function () {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') { // exit using \u0003 - maps to ctrl+c input to exit
+        process.exit();
+      }
+    })
+  }
+  handleUserInput();
+  
+  return stdin;
+};
+
+module.exports = { setupInput }
