040a8342bb7f4ddb44a7a3a3ae3e5040b52591d8
Linh Hoang <ellehoang@hotmail.com>
2021-11-03 22:27:26 -0400

    implement module to connect to node net library, add function to connect to server, and add anonymous function-connect event

---
+++ play.js
@@ -0,0 +1,21 @@
+// client computer
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', //Server IP address
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+          // "connect" is specific event that happens when successful connection is made
+  conn.on("connect", () => { // .on method specify event name & function that does something when event happens
+    conn.write('Name: VN');
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
