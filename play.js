e2b6e41367446a37f98b9f06b9a719daad69c1dc
Linh Hoang <ellehoang@hotmail.com>
2021-11-04 01:46:53 -0400

    remove connection functions, implement setupInput function, and handleUserInput function

--- play.js
+++ play.js
@@ -1,20 +1,22 @@
 // client computer
 const net = require("net");
 const connect = require('./client');
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', //Server IP address
-    port: 50541,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-          // "connect" is specific event that happens when successful connection is made
-  conn.on("connect", () => { // .on method specify event name & function that does something when event happens
-    conn.write('Name: VN');
-  });
-  return conn;
+const setupInput = function () {
+  const stdin = process.stdin; // allows us to listen to keyboard input and react to it
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  const handleUserInput = function () {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') { // exit using \u0003 - maps to ctrl+c input to exit
+        process.exit();
+      }
+    })
+  }
+  handleUserInput();
+  return stdin;
 };
 
 console.log("Connecting ...");
