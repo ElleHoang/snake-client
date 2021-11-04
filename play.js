5cb06ebdf8f932e623669a0f6c4617be2325166f
Linh Hoang <ellehoang@hotmail.com>
2021-11-04 09:23:11 -0400

    move setupInput to input.js and implement connect and setupInput module for import

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
-// client computer
-const net = require("net");
-const connect = require('./client');
-
-const setupInput = function () {
-  const stdin = process.stdin; // allows us to listen to keyboard input and react to it
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  const handleUserInput = function () {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') { // exit using \u0003 - maps to ctrl+c input to exit
-        process.exit();
-      }
-    })
-  }
-  handleUserInput();
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
